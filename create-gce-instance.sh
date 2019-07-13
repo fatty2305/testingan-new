df8f979681924db55b6430789b2d23acf004368e
Tessar <tessar.rama.saputra23@gmail.com>
2019-07-13 05:51:42 +0000

    refactor script to create instance

--- create-gce-instance.sh
+++ create-gce-instance.sh
@@ -7,3 +7,6 @@ export blog_instance_zone="asia-southeast1-a"
 
 gcloud compute instances create $blog_instance_name --zone $blog_instance_zone
 
+wordpress_ip=`gcloud compute instances list | awk '{print $5}' | tail -1`
+
+echo "wordpress blog can be accessed thru : http://$wordpress_ip"
